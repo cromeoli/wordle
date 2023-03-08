c45f04f634a9fb02e38917119103503eedd503e8
cromeoli <cromero@iesrafaelalberti.es>
2023-03-08 02:13:27 +0100

    Vamonos atomos

---
+++ src/test/java/christian/wordle/WordleApplicationTests.java
@@ -0,0 +1,13 @@
+package christian.wordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
